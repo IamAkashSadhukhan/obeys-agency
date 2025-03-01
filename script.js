5f6178c65bebb5af741e419d2d0bfd5ef75cd345
Akash Sadhukhan <akashsadhukhan369@gmail.com>
2025-03-01 18:09:55 +0530

    loader is created

---
+++ script.js
@@ -0,0 +1,42 @@
+
+var tl = gsap.timeline();
+tl.from(".line h1",{
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.5,
+
+});
+tl.from("#line1-part1, .line h2",{
+    opacity: 0,
+    onStart: function(){
+        let h5timer =  document.querySelector("#line1-part1 h5");
+        let grow = 0;
+        setInterval(function(){
+            if(grow < 100) grow++
+            else grow = 100
+            h5timer.innerHTML = grow
+            
+        },33);
+    },
+});
+tl.to(".line h2",{
+    animationName: "anime",
+    opacity: 1,
+})
+tl.to("#loader",{
+    opacity: 0,
+    delay: 4,
+    duration: 0.2,
+});
+tl.from("#page1", {
+    delay:0.2,
+    y: 1600,
+    opacity: 0,
+    duration:0.5,
+    ease:Power4
+})
+tl.to("#loader",{
+    diaplay: "none"
+})
+
