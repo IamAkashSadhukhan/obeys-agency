3a38b84529fe4be36463d2e88f2308bb43aa0cd4
Akash Sadhukhan <akashsadhukhan369@gmail.com>
2025-03-01 21:59:06 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+function loadingAnimation(){  
 var tl = gsap.timeline();
 tl.from(".line h1",{
     y: 150,
@@ -26,7 +26,7 @@ tl.to(".line h2",{
 })
 tl.to("#loader",{
     opacity: 0,
-    delay: 4,
+    delay: 0,
     duration: 0.2,
 });
 tl.from("#page1", {
@@ -39,4 +39,27 @@ tl.from("#page1", {
 tl.to("#loader",{
     diaplay: "none"
 })
+tl.from('#nav',{
+    opacity: 0,
+})
+tl.from("#hero1 h1,#hero2 h1 ,#hero3 h2 , #hero4 h1",{
+    y: 120,
+    stagger: 0.2,
+})
+
+}
+
+function cursorAnimation(){
+    document.addEventListener("mousemove",(val) =>{
+        gsap.to("#crsr",{
+            left: val.x,
+            top:val.y,
+        })
+    })
+    Shery.makeMagnet("#nav-part2 h4");
+}
+loadingAnimation();
+cursorAnimation()
+
+
 
